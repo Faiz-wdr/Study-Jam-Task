7a171af7d61bfa3fe7e52a585cb9628c19429e7a
FaizWdr <fayiz.kappil@gmail.com>
2022-12-02 19:20:31 +0530

    first

---
+++ script.js
@@ -0,0 +1,11 @@
+
+var count=0;
+
+function increment() {
+    var count=number+1;
+    document.getElementById('number').stepUp();
+ }
+ function decrement() {
+    var count=number-1;
+    document.getElementById('number').stepDown();
+ }
